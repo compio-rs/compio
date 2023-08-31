f4553357ad21ddcd2feb12e534522cb5206db343
Yuyi Wang <Strawberry_Str@hotmail.com>
2023-08-31 23:20:25 +0800

    Fix io-uring driver.

--- src/driver/iour/mod.rs
+++ src/driver/iour/mod.rs
@@ -4,7 +4,7 @@ use io_uring::{
     types::{SubmitArgs, Timespec},
     IoUring,
 };
-use std::{io, task::Poll, time::Duration};
+use std::{io, time::Duration};
 
 pub use std::os::fd::{AsRawFd, FromRawFd, IntoRawFd, RawFd};
 
@@ -41,10 +41,13 @@ impl Poller for Driver {
         Ok(())
     }
 
-    unsafe fn push(&self, op: &mut impl OpCode, user_data: usize) -> Poll<io::Result<usize>> {
+    unsafe fn push(&self, op: &mut impl OpCode, user_data: usize) -> io::Result<()> {
         let entry = op.create_entry().user_data(user_data as _);
+        if self.inner.submission_shared().is_full() {
+            self.inner.submit()?;
+        }
         self.inner.submission_shared().push(&entry).unwrap();
-        Poll::Pending
+        Ok(())
     }
 
     fn poll(&self, timeout: Option<Duration>) -> io::Result<Entry> {
@@ -63,10 +66,7 @@ impl Poller for Driver {
         } else {
             Ok(result as _)
         };
-        Ok(Entry {
-            user_data: entry.user_data() as _,
-            result,
-        })
+        Ok(Entry::new(entry.user_data() as _, result))
     }
 }
 
