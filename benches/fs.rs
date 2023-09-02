8f6bea599fa395aadc7e77764928db559a86cdbe
Yuyi Wang <Strawberry_Str@hotmail.com>
2023-09-03 01:14:50 +0800

    Add benches.

---
+++ benches/fs.rs
@@ -0,0 +1,106 @@
+use criterion::{async_executor::AsyncExecutor, criterion_group, criterion_main, Criterion};
+use tempfile::NamedTempFile;
+
+criterion_group!(fs, read, write);
+criterion_main!(fs);
+
+struct CompioRuntime;
+
+impl AsyncExecutor for CompioRuntime {
+    fn block_on<T>(&self, future: impl std::future::Future<Output = T>) -> T {
+        compio::task::block_on(future)
+    }
+}
+
+fn read(c: &mut Criterion) {
+    let mut group = c.benchmark_group("read");
+
+    group.bench_function("std", |b| {
+        b.iter(|| {
+            use std::io::Read;
+
+            let mut file = std::fs::File::open("Cargo.toml").unwrap();
+            let mut buffer = Vec::with_capacity(1024);
+            file.read_to_end(&mut buffer).unwrap();
+            buffer
+        })
+    });
+
+    group.bench_function("tokio", |b| {
+        let runtime = tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+        b.to_async(&runtime).iter(|| async {
+            use tokio::io::AsyncReadExt;
+
+            let mut file = tokio::fs::File::open("Cargo.toml").await.unwrap();
+            let mut buffer = Vec::with_capacity(1024);
+            file.read_to_end(&mut buffer).await.unwrap();
+            buffer
+        })
+    });
+
+    group.bench_function("iocp", |b| {
+        b.to_async(CompioRuntime).iter(|| async {
+            let file = compio::fs::File::open("Cargo.toml").unwrap();
+            let mut buffer = Vec::with_capacity(1024);
+            loop {
+                let old_len = buffer.len();
+                let (n, sbuf) = file.read_at(buffer, old_len).await;
+                buffer = sbuf;
+                let n = n.unwrap();
+                if n == 0 {
+                    break;
+                }
+            }
+            buffer
+        })
+    });
+
+    group.finish();
+}
+
+static CONTENT: &[u8] = include_bytes!("../Cargo.toml");
+
+fn write(c: &mut Criterion) {
+    let mut group = c.benchmark_group("write");
+
+    group.bench_function("std", |b| {
+        let temp_file = NamedTempFile::new().unwrap();
+        b.iter(|| {
+            use std::io::Write;
+
+            let mut file = std::fs::File::create(temp_file.path()).unwrap();
+            file.write_all(CONTENT).unwrap();
+        })
+    });
+
+    group.bench_function("tokio", |b| {
+        let runtime = tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+        let temp_file = NamedTempFile::new().unwrap();
+        b.to_async(&runtime).iter(|| async {
+            use tokio::io::AsyncWriteExt;
+
+            let mut file = tokio::fs::File::create(temp_file.path()).await.unwrap();
+            file.write_all(CONTENT).await.unwrap();
+        })
+    });
+
+    group.bench_function("compio", |b| {
+        let temp_file = NamedTempFile::new().unwrap();
+        b.to_async(CompioRuntime).iter(|| async {
+            let file = compio::fs::File::create(temp_file.path()).unwrap();
+            let mut pos = 0;
+            while pos < CONTENT.len() {
+                let (res, _) = file.write_at(&CONTENT[pos..], pos).await;
+                pos += res.unwrap();
+            }
+        })
+    });
+
+    group.finish()
+}
---
+++ benches/net.rs
@@ -0,0 +1,140 @@
+use criterion::{async_executor::AsyncExecutor, criterion_group, criterion_main, Criterion};
+
+criterion_group!(net, tcp, udp);
+criterion_main!(net);
+
+struct CompioRuntime;
+
+impl AsyncExecutor for CompioRuntime {
+    fn block_on<T>(&self, future: impl std::future::Future<Output = T>) -> T {
+        compio::task::block_on(future)
+    }
+}
+
+fn tcp(c: &mut Criterion) {
+    const PACKET_LEN: usize = 1048576;
+    static PACKET: &[u8] = &[1u8; PACKET_LEN];
+
+    let mut group = c.benchmark_group("tcp");
+
+    group.bench_function("tokio", |b| {
+        let runtime = tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+        b.to_async(&runtime).iter(|| async {
+            use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+            let listener = tokio::net::TcpListener::bind("127.0.0.1:0").await.unwrap();
+            let addr = listener.local_addr().unwrap();
+            let tx = tokio::net::TcpStream::connect(addr);
+            let rx = listener.accept();
+            let (mut tx, (mut rx, _)) = tokio::try_join!(tx, rx).unwrap();
+            tx.write_all(PACKET).await.unwrap();
+            let mut buffer = Vec::with_capacity(PACKET_LEN);
+            while buffer.len() < PACKET_LEN {
+                rx.read_buf(&mut buffer).await.unwrap();
+            }
+            buffer
+        })
+    });
+
+    group.bench_function("compio", |b| {
+        b.to_async(CompioRuntime).iter(|| async {
+            let listener = compio::net::TcpListener::bind("127.0.0.1:0").unwrap();
+            let addr = listener.local_addr().unwrap();
+            let tx = compio::net::TcpStream::connect(addr);
+            let rx = listener.accept();
+            let (tx, (rx, _)) = futures_util::try_join!(tx, rx).unwrap();
+            {
+                let mut pos = 0;
+                while pos < PACKET_LEN {
+                    let (res, _) = tx.send(&PACKET[pos..]).await;
+                    pos += res.unwrap();
+                }
+            }
+            {
+                let mut buffer = Vec::with_capacity(PACKET_LEN);
+                let mut res;
+                while buffer.len() < PACKET_LEN {
+                    (res, buffer) = rx.recv(buffer).await;
+                    res.unwrap();
+                }
+                buffer
+            }
+        })
+    });
+
+    group.finish();
+}
+
+fn udp(c: &mut Criterion) {
+    const PACKET_LEN: usize = 1024;
+    static PACKET: &[u8] = &[1u8; PACKET_LEN];
+
+    let mut group = c.benchmark_group("udp");
+
+    group.bench_function("tokio", |b| {
+        let runtime = tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+        b.to_async(&runtime).iter(|| async {
+            let rx = tokio::net::UdpSocket::bind("127.0.0.1:0").await.unwrap();
+            let addr_rx = rx.local_addr().unwrap();
+            let tx = tokio::net::UdpSocket::bind("127.0.0.1:0").await.unwrap();
+            let addr_tx = tx.local_addr().unwrap();
+
+            rx.connect(addr_tx).await.unwrap();
+            tx.connect(addr_rx).await.unwrap();
+
+            {
+                let mut pos = 0;
+                while pos < PACKET_LEN {
+                    let res = tx.send(&PACKET[pos..]).await;
+                    pos += res.unwrap();
+                }
+            }
+            {
+                let mut buffer = vec![0; PACKET_LEN];
+                let mut pos = 0;
+                while pos < PACKET_LEN {
+                    let res = rx.recv(&mut buffer[pos..]).await;
+                    pos += res.unwrap();
+                }
+                buffer
+            }
+        })
+    });
+
+    group.bench_function("compio", |b| {
+        b.to_async(CompioRuntime).iter(|| async {
+            let rx = compio::net::UdpSocket::bind("127.0.0.1:0").unwrap();
+            let addr_rx = rx.local_addr().unwrap();
+            let tx = compio::net::UdpSocket::bind("127.0.0.1:0").unwrap();
+            let addr_tx = tx.local_addr().unwrap();
+
+            rx.connect(addr_tx).unwrap();
+            tx.connect(addr_rx).unwrap();
+
+            {
+                let mut pos = 0;
+                while pos < PACKET_LEN {
+                    let (res, _) = tx.send(&PACKET[pos..]).await;
+                    pos += res.unwrap();
+                }
+            }
+            {
+                let mut buffer = Vec::with_capacity(PACKET_LEN);
+                let mut res;
+                while buffer.len() < PACKET_LEN {
+                    (res, buffer) = rx.recv(buffer).await;
+                    res.unwrap();
+                }
+                buffer
+            }
+        })
+    });
+
+    group.finish();
+}
