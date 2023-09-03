7a08094a3a752e2a43d43dc646ce641e60cdeea8
Yuyi Wang <Strawberry_Str@hotmail.com>
2023-09-03 10:49:12 +0800

    Add ctrlc to tick example.

--- examples/tick.rs
+++ examples/tick.rs
@@ -1,12 +1,19 @@
+use async_ctrlc::CtrlC;
 use compio::time::interval;
+use futures_util::{select, FutureExt};
 use std::time::Duration;
 
 fn main() {
     compio::task::block_on(async {
         let mut interval = interval(Duration::from_secs(1));
+        let mut ctrlc = CtrlC::new().unwrap();
         loop {
-            interval.tick().await;
-            println!("ping");
+            let ctrlc = std::pin::pin!(&mut ctrlc);
+            select! {
+                _ = ctrlc.fuse() => break,
+                _ = interval.tick().fuse() => println!("ping"),
+            }
         }
+        println!("exit");
     })
 }
