81cf73e684598a8cdafd5e628226091ecd8ef09b
Yuyi Wang <Strawberry_Str@hotmail.com>
2023-09-04 12:58:09 +0800

    Impl cqes for io-uring.

--- src/driver/iour/mod.rs
+++ src/driver/iour/mod.rs
@@ -5,7 +5,7 @@ use io_uring::{
     types::{Fd, SubmitArgs, Timespec},
     IoUring,
 };
-use std::{cell::UnsafeCell, io, marker::PhantomData, time::Duration};
+use std::{cell::UnsafeCell, io, marker::PhantomData, mem::MaybeUninit, time::Duration};
 
 pub use libc::{sockaddr_storage, socklen_t};
 pub use std::os::fd::{AsRawFd, FromRawFd, IntoRawFd, RawFd};
@@ -71,7 +71,22 @@ impl Poller for Driver {
         unsafe { self.push_entry(entry) }
     }
 
-    fn poll(&self, timeout: Option<Duration>) -> io::Result<Entry> {
+    fn poll(
+        &self,
+        timeout: Option<Duration>,
+        entries: &mut [MaybeUninit<Entry>],
+    ) -> io::Result<usize> {
+        {
+            let mut cqe = unsafe { self.inner.completion_shared() };
+            if !cqe.is_empty() {
+                let len = entries.len().min(cqe.len());
+                for entry in entries {
+                    let iour_entry = cqe.next().unwrap();
+                    entry.write(create_entry(iour_entry));
+                }
+                return Ok(len);
+            }
+        }
         if let Some(duration) = timeout {
             let timespec = timespec(duration);
             let args = SubmitArgs::new().timespec(&timespec);
@@ -89,15 +104,23 @@ impl Poller for Driver {
             // Submit and Wait without timeout
             self.inner.submit_and_wait(1)?;
         }
-        let entry = unsafe { self.inner.completion_shared() }.next().unwrap();
-        let result = entry.result();
-        let result = if result < 0 {
-            Err(io::Error::from_raw_os_error(-result))
-        } else {
-            Ok(result as _)
-        };
-        Ok(Entry::new(entry.user_data() as _, result))
+        let cqe = unsafe { self.inner.completion_shared() };
+        let len = cqe.len();
+        for (iour_entry, entry) in cqe.zip(entries) {
+            entry.write(create_entry(iour_entry));
+        }
+        Ok(len)
     }
+}
+
+fn create_entry(entry: io_uring::cqueue::Entry) -> Entry {
+    let result = entry.result();
+    let result = if result < 0 {
+        Err(io::Error::from_raw_os_error(-result))
+    } else {
+        Ok(result as _)
+    };
+    Entry::new(entry.user_data() as _, result)
 }
 
 fn timespec(duration: std::time::Duration) -> Timespec {
