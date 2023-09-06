6e03f2e686bc24b5c69f15947b1c2bac16650f05
王宇逸 <Strawberry_Str@hotmail.com>
2023-09-06 23:18:52 +0800

    Adapt to removal of FileInner.

--- src/driver/iour/fs.rs
+++ src/driver/iour/fs.rs
@@ -1,35 +1,33 @@
 use std::{io, path::Path};
 
-use crate::driver::{AsRawFd, FromRawFd, IntoRawFd, RawFd};
+use crate::{
+    driver::{AsRawFd, FromRawFd, IntoRawFd, RawFd},
+    fs::File,
+};
 
-pub struct FileInner {
-    file: std::fs::File,
+pub fn file_with_options(
+    path: impl AsRef<Path>,
+    options: std::fs::OpenOptions,
+) -> io::Result<std::fs::File> {
+    options.open(path)
 }
 
-impl FileInner {
-    pub fn with_options(path: impl AsRef<Path>, options: std::fs::OpenOptions) -> io::Result<Self> {
-        Ok(Self {
-            file: options.open(path)?,
-        })
-    }
-}
-
-impl AsRawFd for FileInner {
+impl AsRawFd for File {
     fn as_raw_fd(&self) -> RawFd {
-        self.file.as_raw_fd()
+        self.inner.as_raw_fd()
     }
 }
 
-impl FromRawFd for FileInner {
+impl FromRawFd for File {
     unsafe fn from_raw_fd(fd: RawFd) -> Self {
         Self {
-            file: std::fs::File::from_raw_fd(fd),
+            inner: std::fs::File::from_raw_fd(fd),
         }
     }
 }
 
-impl IntoRawFd for FileInner {
+impl IntoRawFd for File {
     fn into_raw_fd(self) -> RawFd {
-        self.file.into_raw_fd()
+        self.inner.into_raw_fd()
     }
 }
