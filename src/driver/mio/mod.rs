b9a901e852a783a5e667db5b350ce33ae2b51f6e
George Miao <gm@miao.dev>
2023-09-09 02:03:22 -0400

    feat: mio driver & op structure

--- src/driver/mio/mod.rs
+++ src/driver/mio/mod.rs
@@ -2,6 +2,7 @@
 pub use std::os::fd::{AsRawFd, FromRawFd, IntoRawFd, RawFd};
 use std::{cell::RefCell, io, mem::MaybeUninit, ops::DerefMut, time::Duration};
 
+pub(crate) use libc::{sockaddr_storage, socklen_t};
 use mio::{
     event::{Event, Source},
     unix::SourceFd,
@@ -15,11 +16,66 @@ pub(crate) mod fs;
 pub(crate) mod net;
 pub(crate) mod op;
 
-/// Abstraction of mio operations.
+/// Helper macro to execute a system call that returns an `io::Result`.
+macro_rules! syscall {
+    ($fn: ident ( $($arg: expr),* $(,)* ) ) => {{
+        #[allow(unused_unsafe)]
+        let res = unsafe { libc::$fn($($arg, )*) };
+        if res == -1 {
+            Err(std::io::Error::last_os_error())
+        } else {
+            Ok(res as _)
+        }
+    }};
+}
+
+pub(crate) use syscall;
+
+/// Abstraction of operations.
 pub trait OpCode {
-    fn interests(&self) -> Interest;
-    fn source_fd(&self) -> SourceFd<'_>;
-    fn perform(&mut self, event: &Event) -> io::Result<usize>;
+    /// Perform the operation before submit, and return [`Decision`] to
+    /// indicate whether submitting the operation to mio is required.
+    fn pre_submit(&mut self) -> io::Result<Decision>;
+
+    /// Perform the operation after received corresponding
+    /// event.
+    fn on_event(&mut self, event: &Event) -> io::Result<usize>;
+}
+
+/// Result of [`OpCode::pre_submit`].
+pub enum Decision {
+    /// Instant operation, no need to submit
+    Completed(usize),
+    /// Async operation, needs to submit
+    Wait(OpMeta),
+}
+
+impl Decision {
+    /// Decide to complete the operation with the given result.
+    pub fn complete(result: usize) -> Self {
+        Self::Completed(result)
+    }
+
+    /// Decide to wait for the given fd with the given interest.
+    pub fn wait_for(fd: RawFd, interest: Interest) -> Self {
+        Self::Wait(OpMeta { fd, interest })
+    }
+
+    /// Decide to wait for the given fd to be readable.
+    pub fn wait_readable(fd: RawFd) -> Self {
+        Self::wait_for(fd, Interest::READABLE)
+    }
+
+    /// Decide to wait for the given fd to be writable.
+    pub fn wait_writable(fd: RawFd) -> Self {
+        Self::wait_for(fd, Interest::WRITABLE)
+    }
+}
+
+/// Meta of mio operations.
+pub struct OpMeta {
+    fd: RawFd,
+    interest: Interest,
 }
 
 /// Low-level driver of mio.
@@ -30,12 +86,13 @@ pub struct Driver {
 }
 
 /// Inner state of [`Driver`].
-pub struct DriverInner {
+struct DriverInner {
     events: Events,
     poll: Poll,
     registered: Slab<MioEntry>,
 }
 
+/// Internal representation of operation being submitted into the driver.
 struct MioEntry {
     op: *mut dyn OpCode,
     user_data: usize,
@@ -51,7 +108,7 @@ impl MioEntry {
         }
     }
 
-    fn op_mut(&self) -> &mut dyn OpCode {
+    fn op_mut(&mut self) -> &mut dyn OpCode {
         unsafe { &mut *self.op }
     }
 
@@ -84,8 +141,18 @@ impl Driver {
     /// Register all operations in the squeue to mio.
     fn submit_squeue(&self) -> io::Result<()> {
         self.with_inner(|inner| {
-            while let Some(entry) = self.squeue.pop() {
-                inner.submit(entry)?;
+            while let Some(mut entry) = self.squeue.pop() {
+                match entry.op_mut().pre_submit() {
+                    Ok(Decision::Wait(meta)) => {
+                        inner.submit(entry, meta)?;
+                    }
+                    Ok(Decision::Completed(res)) => {
+                        self.cqueue.push(Entry::new(entry.user_data, Ok(res)));
+                    }
+                    Err(err) => {
+                        self.cqueue.push(Entry::new(entry.user_data, Err(err)));
+                    }
+                }
             }
 
             Ok(())
@@ -100,19 +167,19 @@ impl Driver {
 
             for event in &inner.events {
                 let token = event.token();
-                let entry = inner
+                let mut entry = inner
                     .registered
                     .try_remove(token.0)
-                    .expect("Unknown token returned by mio");
-                let res = entry.op_mut().perform(event);
+                    .expect("Unknown token returned by mio"); // XXX: Should this be silently ignored?
+                let res = entry.op_mut().on_event(event);
 
-                self.cqueue.push(Entry::new(token.into(), res))
+                self.cqueue.push(Entry::new(entry.user_data, res))
             }
             Ok(())
         })
     }
 
-    fn get_completed(&self, entries: &mut [MaybeUninit<Entry>]) -> usize {
+    fn poll_completed(&self, entries: &mut [MaybeUninit<Entry>]) -> usize {
         let len = self.cqueue.len().min(entries.len());
         for entry in &mut entries[..len] {
             entry.write(self.cqueue.pop().unwrap());
@@ -129,14 +196,11 @@ impl Driver {
 }
 
 impl DriverInner {
-    fn submit(&mut self, entry: MioEntry) -> io::Result<()> {
+    fn submit(&mut self, entry: MioEntry, meta: OpMeta) -> io::Result<()> {
         let slot = self.registered.vacant_entry();
         let token = Token(slot.key());
 
-        entry
-            .op()
-            .source_fd()
-            .register(self.poll.registry(), token, entry.op().interests())?;
+        SourceFd(&meta.fd).register(self.poll.registry(), token, meta.interest)?;
 
         // Only insert the entry after it was registered successfully
         slot.insert(entry);
@@ -156,7 +220,8 @@ impl Poller for Driver {
     }
 
     fn post(&self, user_data: usize, result: usize) -> io::Result<()> {
-        todo!()
+        self.cqueue.push(Entry::new(user_data, Ok(result)));
+        Ok(())
     }
 
     fn poll(
@@ -168,10 +233,10 @@ impl Poller for Driver {
         if entries.is_empty() {
             return Ok(0);
         }
-        if self.get_completed(entries) > 0 {
+        if self.poll_completed(entries) > 0 {
             return Ok(entries.len());
         }
         self.poll(timeout)?;
-        Ok(self.get_completed(entries))
+        Ok(self.poll_completed(entries))
     }
 }
--- src/driver/mio/op.rs
+++ src/driver/mio/op.rs
@@ -1,1 +1,101 @@
+use std::io;
 
+use mio::event::Event;
+
+pub use crate::driver::unix_op::*;
+use crate::{
+    buf::{AsIoSlices, AsIoSlicesMut, IoBuf, IoBufMut},
+    driver::{syscall, Decision, OpCode},
+    op::*,
+};
+
+impl<T: IoBufMut> OpCode for ReadAt<T> {
+    fn pre_submit(&mut self) -> io::Result<Decision> {
+        Ok(Decision::wait_readable(self.fd))
+    }
+
+    fn on_event(&mut self, event: &Event) -> std::io::Result<usize> {
+        todo!()
+    }
+}
+
+impl<T: IoBuf> OpCode for WriteAt<T> {
+    fn pre_submit(&mut self) -> io::Result<Decision> {
+        Ok(Decision::wait_writable(self.fd))
+    }
+
+    fn on_event(&mut self, event: &Event) -> std::io::Result<usize> {
+        todo!()
+    }
+}
+
+impl OpCode for Sync {
+    fn pre_submit(&mut self) -> io::Result<Decision> {
+        Ok(Decision::Completed(0))
+    }
+
+    fn on_event(&mut self, event: &Event) -> std::io::Result<usize> {
+        unreachable!("Sync operation should not be submitted to mio")
+    }
+}
+
+impl OpCode for Accept {
+    fn pre_submit(&mut self) -> io::Result<Decision> {
+        Ok(Decision::wait_readable(self.fd))
+    }
+
+    fn on_event(&mut self, event: &Event) -> std::io::Result<usize> {
+        todo!()
+    }
+}
+
+impl OpCode for Connect {
+    fn pre_submit(&mut self) -> io::Result<Decision> {
+        let res = syscall!(connect(self.fd, self.addr.as_ptr(), self.addr.len(),))?;
+        Ok(Decision::Completed(res))
+    }
+
+    fn on_event(&mut self, event: &Event) -> std::io::Result<usize> {
+        todo!()
+    }
+}
+
+impl<T: AsIoSlicesMut> OpCode for RecvImpl<T> {
+    fn pre_submit(&mut self) -> io::Result<Decision> {
+        Ok(Decision::wait_readable(self.fd))
+    }
+
+    fn on_event(&mut self, event: &Event) -> std::io::Result<usize> {
+        todo!()
+    }
+}
+
+impl<T: AsIoSlices> OpCode for SendImpl<T> {
+    fn pre_submit(&mut self) -> io::Result<Decision> {
+        Ok(Decision::wait_writable(self.fd))
+    }
+
+    fn on_event(&mut self, event: &Event) -> std::io::Result<usize> {
+        todo!()
+    }
+}
+
+impl<T: AsIoSlicesMut> OpCode for RecvFromImpl<T> {
+    fn pre_submit(&mut self) -> io::Result<Decision> {
+        Ok(Decision::Completed(0))
+    }
+
+    fn on_event(&mut self, event: &Event) -> std::io::Result<usize> {
+        todo!()
+    }
+}
+
+impl<T: AsIoSlices> OpCode for SendToImpl<T> {
+    fn pre_submit(&mut self) -> io::Result<Decision> {
+        Ok(Decision::Completed(0))
+    }
+
+    fn on_event(&mut self, event: &Event) -> std::io::Result<usize> {
+        todo!()
+    }
+}
