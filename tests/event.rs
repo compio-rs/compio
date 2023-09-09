73d2893b73cfeb5a737c64d7102c01d988084bc9
王宇逸 <Strawberry_Str@hotmail.com>
2023-09-09 14:41:42 +0800

    Add event test.

---
+++ tests/event.rs
@@ -0,0 +1,13 @@
+use compio::event::Event;
+
+#[test]
+fn event_handle() {
+    compio::task::block_on(async {
+        let event = Event::new().unwrap();
+        let handle = event.handle();
+        std::thread::scope(|scope| {
+            scope.spawn(|| handle.notify().unwrap());
+        });
+        event.wait().await.unwrap();
+    });
+}
