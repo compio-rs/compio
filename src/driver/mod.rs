8ef8e2a381ad7d207991e3c5c3de02fd0433aaba
George Miao <gm@miao.dev>
2023-09-10 05:14:59 -0400

    refactor: remove unnecessary cfg
    Co-authored-by: 王宇逸 <Strawberry_Str@hotmail.com>

--- src/driver/mod.rs
+++ src/driver/mod.rs
@@ -12,7 +12,7 @@ cfg_if::cfg_if! {
     } else if #[cfg(target_os = "linux")] {
         mod iour;
         pub use iour::*;
-    } else if #[cfg(all(not(target_os = "linux"), unix))]{
+    } else if #[cfg(unix)]{
         mod mio;
         pub use self::mio::*;
     }
