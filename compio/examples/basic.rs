8d1ee34c6b2fb037de2d06408825a3ffed56592c
kompl3xpr <kompl3xpr@proton.me>
2023-09-28 17:36:13 +0800

    Move `compio-macros` declaration to workspace

--- compio/examples/basic.rs
+++ compio/examples/basic.rs
@@ -1,12 +1,11 @@
 use compio::fs::OpenOptions;
 
-fn main() {
-    let buffer = compio::task::block_on(async {
-        let file = OpenOptions::new().read(true).open("Cargo.toml").unwrap();
-        let (read, buffer) = file.read_to_end_at(Vec::with_capacity(4096), 0).await;
-        let read = read.unwrap();
-        assert_eq!(read, buffer.len());
-        String::from_utf8(buffer).unwrap()
-    });
+#[compio::main]
+async fn main() {
+    let file = OpenOptions::new().read(true).open("Cargo.toml").unwrap();
+    let (read, buffer) = file.read_to_end_at(Vec::with_capacity(4096), 0).await;
+    let read = read.unwrap();
+    assert_eq!(read, buffer.len());
+    let buffer = String::from_utf8(buffer).unwrap();
     println!("{}", buffer);
 }
