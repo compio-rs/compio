46a657d3f0e41a955e30dfedbabbb125429f19d4
kompl3xpr <kompl3xpr@proton.me>
2023-09-28 18:49:40 +0800

    Implement `compio::test` macro

--- compio-macros/src/lib.rs
+++ compio-macros/src/lib.rs
@@ -1,15 +1,22 @@
 mod main_fn;
 use main_fn::CompioMain;
 
+mod test_fn;
+use proc_macro::TokenStream;
 use quote::ToTokens;
 use syn::parse_macro_input;
+use test_fn::CompioTest;
 
 #[proc_macro_attribute]
-pub fn main(
-    _args: proc_macro::TokenStream,
-    item: proc_macro::TokenStream,
-) -> proc_macro::TokenStream {
+pub fn main(_args: TokenStream, item: TokenStream) -> TokenStream {
     parse_macro_input!(item as CompioMain)
+        .into_token_stream()
+        .into()
+}
+
+#[proc_macro_attribute]
+pub fn test(_args: TokenStream, item: TokenStream) -> TokenStream {
+    parse_macro_input!(item as CompioTest)
         .into_token_stream()
         .into()
 }
---
+++ compio-macros/src/test_fn.rs
@@ -0,0 +1,50 @@
+use proc_macro2::{Ident, Span, TokenStream};
+use quote::{quote, ToTokens, TokenStreamExt};
+use syn::{parse::Parse, AttrStyle, Attribute, Signature, Visibility};
+
+pub(crate) struct CompioTest {
+    pub attrs: Vec<Attribute>,
+    pub vis: Visibility,
+    pub sig: Signature,
+    pub block: TokenStream,
+}
+
+impl Parse for CompioTest {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let outer_attrs = input.call(Attribute::parse_outer)?;
+        let vis: Visibility = input.parse()?;
+        let mut sig: Signature = input.parse()?;
+        let block: TokenStream = input.parse()?;
+
+        if let None = sig.asyncness {
+            return Err(syn::Error::new_spanned(
+                sig.ident,
+                "the `async` keyword is missing from the function declaration",
+            ));
+        };
+        sig.asyncness.take();
+        Ok(Self {
+            attrs: outer_attrs,
+            vis,
+            sig,
+            block,
+        })
+    }
+}
+
+impl ToTokens for CompioTest {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        tokens.append_all(quote!(#[test]));
+        tokens.append_all(
+            self.attrs
+                .iter()
+                .filter(|a| matches!(a.style, AttrStyle::Outer)),
+        );
+        self.vis.to_tokens(tokens);
+        self.sig.to_tokens(tokens);
+        let body = &self.block;
+        tokens.append_all(quote!({
+            compio::task::block_on(async move #body)
+        }));
+    }
+}
