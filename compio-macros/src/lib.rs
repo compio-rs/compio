718e54a23780a0ba687fc7d0d3c99b902be03573
kompl3xpr <kompl3xpr@proton.me>
2023-09-28 18:40:43 +0800

    Update module structure in `compio-macros`

--- compio-macros/src/lib.rs
+++ compio-macros/src/lib.rs
@@ -1,59 +1,8 @@
-use proc_macro2::{Ident, Span, TokenStream};
-use quote::{quote, ToTokens, TokenStreamExt};
-use syn::{parse::Parse, parse_macro_input, AttrStyle, Attribute, Signature, Visibility};
+mod main_fn;
+use main_fn::CompioMain;
 
-struct CompioMain {
-    pub attrs: Vec<Attribute>,
-    pub vis: Visibility,
-    pub sig: Signature,
-    pub block: TokenStream,
-}
-
-impl Parse for CompioMain {
-    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
-        let outer_attrs = input.call(Attribute::parse_outer)?;
-        let vis: Visibility = input.parse()?;
-        let mut sig: Signature = input.parse()?;
-        let block: TokenStream = input.parse()?;
-
-        if let None = sig.asyncness {
-            return Err(syn::Error::new_spanned(
-                sig.ident,
-                "the `async` keyword is missing from the function declaration",
-            ));
-        };
-        if sig.ident != Ident::new("main", Span::call_site()) {
-            return Err(syn::Error::new_spanned(
-                sig.ident,
-                "`compio::main` can only be used for main function.",
-            ));
-        }
-
-        sig.asyncness.take();
-        Ok(Self {
-            attrs: outer_attrs,
-            vis,
-            sig,
-            block,
-        })
-    }
-}
-
-impl ToTokens for CompioMain {
-    fn to_tokens(&self, tokens: &mut TokenStream) {
-        tokens.append_all(
-            self.attrs
-                .iter()
-                .filter(|a| matches!(a.style, AttrStyle::Outer)),
-        );
-        self.vis.to_tokens(tokens);
-        self.sig.to_tokens(tokens);
-        let body = &self.block;
-        tokens.append_all(quote!({
-            compio::task::block_on(async move #body)
-        }));
-    }
-}
+use quote::ToTokens;
+use syn::parse_macro_input;
 
 #[proc_macro_attribute]
 pub fn main(
