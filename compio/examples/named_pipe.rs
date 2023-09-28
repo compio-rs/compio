8143cb5dab8c669bae5f0f482a54b5ec38e02458
kompl3xpr <kompl3xpr@proton.me>
2023-09-28 18:13:08 +0800

    Update examples

--- compio/examples/named_pipe.rs
+++ compio/examples/named_pipe.rs
@@ -1,27 +1,26 @@
-fn main() {
+#[compio::main]
+async fn main() {
     #[cfg(target_os = "windows")]
     {
         use compio::named_pipe::{ClientOptions, ServerOptions};
 
         const PIPE_NAME: &str = r"\\.\pipe\compio-named-pipe";
 
-        compio::task::block_on(async {
-            let server = ServerOptions::new()
-                .access_inbound(false)
-                .create(PIPE_NAME)
-                .unwrap();
-            let client = ClientOptions::new().write(false).open(PIPE_NAME).unwrap();
+        let server = ServerOptions::new()
+            .access_inbound(false)
+            .create(PIPE_NAME)
+            .unwrap();
+        let client = ClientOptions::new().write(false).open(PIPE_NAME).unwrap();
 
-            server.connect().await.unwrap();
+        server.connect().await.unwrap();
 
-            let write = server.write_all("Hello world!");
-            let buffer = Vec::with_capacity(12);
-            let read = client.read_exact(buffer);
+        let write = server.write_all("Hello world!");
+        let buffer = Vec::with_capacity(12);
+        let read = client.read_exact(buffer);
 
-            let ((write, _), (read, buffer)) = futures_util::join!(write, read);
-            write.unwrap();
-            read.unwrap();
-            println!("{}", String::from_utf8(buffer).unwrap());
-        });
+        let ((write, _), (read, buffer)) = futures_util::join!(write, read);
+        write.unwrap();
+        read.unwrap();
+        println!("{}", String::from_utf8(buffer).unwrap());
     }
 }
--- compio/examples/net.rs
+++ compio/examples/net.rs
@@ -2,19 +2,18 @@ use std::net::Ipv4Addr;
 
 use compio::net::{TcpListener, TcpStream};
 
-fn main() {
-    compio::task::block_on(async {
-        let listener = TcpListener::bind((Ipv4Addr::LOCALHOST, 0)).unwrap();
-        let addr = listener.local_addr().unwrap();
+#[compio::main]
+async fn main() {
+    let listener = TcpListener::bind((Ipv4Addr::LOCALHOST, 0)).unwrap();
+    let addr = listener.local_addr().unwrap();
 
-        let (tx, (rx, _)) =
-            futures_util::try_join!(TcpStream::connect(&addr), listener.accept()).unwrap();
+    let (tx, (rx, _)) =
+        futures_util::try_join!(TcpStream::connect(&addr), listener.accept()).unwrap();
 
-        tx.send_all("Hello world!").await.0.unwrap();
+    tx.send_all("Hello world!").await.0.unwrap();
 
-        let buffer = Vec::with_capacity(12);
-        let (n, buffer) = rx.recv_exact(buffer).await;
-        assert_eq!(n.unwrap(), buffer.len());
-        println!("{}", String::from_utf8(buffer).unwrap());
-    });
+    let buffer = Vec::with_capacity(12);
+    let (n, buffer) = rx.recv_exact(buffer).await;
+    assert_eq!(n.unwrap(), buffer.len());
+    println!("{}", String::from_utf8(buffer).unwrap());
 }
