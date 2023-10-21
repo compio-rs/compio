8e0434c829819be9e929c55a3413b21bc03f4b8c
王宇逸 <Strawberry_Str@hotmail.com>
2023-10-21 22:34:41 +0800

    docs(net): add example for async resolve.

---
+++ compio/examples/resolve.rs
@@ -0,0 +1,21 @@
+use compio::net::ToSocketAddrsAsync;
+use futures_util::{stream::FuturesUnordered, StreamExt};
+
+#[compio::main(crate = "compio")]
+async fn main() {
+    let mut tasks = std::env::args()
+        .skip(1)
+        .map(|name| async move {
+            (
+                (name.as_str(), 0).to_socket_addrs_async().await.unwrap(),
+                name,
+            )
+        })
+        .collect::<FuturesUnordered<_>>();
+    while let Some((addrs, name)) = tasks.next().await {
+        println!("Address of {}", name);
+        for addr in addrs {
+            println!("    {}", addr.ip());
+        }
+    }
+}
