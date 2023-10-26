68eef0023856712b77e13dcbf2231840d65df725
王宇逸 <Strawberry_Str@hotmail.com>
2023-10-26 15:49:38 +0800

    fix: use hyper body to_bytes

--- compio-http/examples/client.rs
+++ compio-http/examples/client.rs
@@ -1,5 +1,5 @@
 use compio_http::Client;
-use hyper::{body::HttpBody, Method};
+use hyper::Method;
 
 #[compio_macros::main]
 async fn main() {
@@ -8,10 +8,10 @@ async fn main() {
         .request(Method::GET, "https://www.example.com/".parse().unwrap())
         .await
         .unwrap();
-    let (parts, mut body) = response.into_parts();
+    let (parts, body) = response.into_parts();
     println!("{:?}", parts);
     println!(
         "{}",
-        std::str::from_utf8(&body.data().await.unwrap().unwrap()).unwrap()
+        std::str::from_utf8(&hyper::body::to_bytes(body).await.unwrap()).unwrap()
     );
 }
