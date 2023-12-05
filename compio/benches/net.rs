8669550805cd0ae77246793ebcd5be5b581f7716
Ho 229 <ho229v3666@gmail.com>
2023-12-05 20:56:28 +0800

    feat: add multi task net benchmark

--- compio/benches/net.rs
+++ compio/benches/net.rs
@@ -1,6 +1,8 @@
+use std::sync::Arc;
+
 use criterion::{criterion_group, criterion_main, Criterion};
 
-criterion_group!(net, tcp, udp);
+criterion_group!(net, tcp, udp, multi_tcp, multi_udp);
 criterion_main!(net);
 
 fn tcp(c: &mut Criterion) {
@@ -177,6 +179,254 @@ fn udp(c: &mut Criterion) {
                         buffer = rx.recv(buffer).await.1;
                     }
                     buffer
+                }
+            })
+        })
+    });
+
+    group.finish();
+}
+
+fn multi_tcp(c: &mut Criterion) {
+    const TASK_COUNT: u32 = 100;
+    const PACKET_LEN: usize = 1048576;
+    static PACKET: &[u8] = &[1u8; PACKET_LEN];
+
+    let mut group = c.benchmark_group("multi_tcp");
+
+    group.sample_size(50);
+
+    group.bench_function("tokio", |b| {
+        let runtime = tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+        b.to_async(&runtime).iter(|| async {
+            use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+            let listener = Arc::new(tokio::net::TcpListener::bind("127.0.0.1:0").await.unwrap());
+
+            let task = |listener: Arc<tokio::net::TcpListener>| async move {
+                let addr = listener.local_addr().unwrap();
+                let tx = tokio::net::TcpStream::connect(addr);
+                let rx = listener.accept();
+                let (mut tx, (mut rx, _)) = tokio::try_join!(tx, rx).unwrap();
+
+                tx.write_all(PACKET).await.unwrap();
+                let mut buffer = Vec::with_capacity(PACKET_LEN);
+                while buffer.len() < PACKET_LEN {
+                    rx.read_buf(&mut buffer).await.unwrap();
+                }
+            };
+
+            let handles = (0..TASK_COUNT)
+                .map(|_| tokio::spawn(task(listener.clone())))
+                .collect::<Vec<_>>();
+
+            for handle in handles {
+                handle.await.unwrap();
+            }
+        })
+    });
+
+    group.bench_function("compio", |b| {
+        let runtime = compio::runtime::Runtime::new().unwrap();
+        b.to_async(&runtime).iter(|| async {
+            use compio::io::{AsyncReadExt, AsyncWriteExt};
+
+            let listener = Arc::new(compio::net::TcpListener::bind("127.0.0.1:0").await.unwrap());
+
+            let task = |listener: Arc<compio::net::TcpListener>| async move {
+                let addr = listener.local_addr().unwrap();
+                let tx = compio::net::TcpStream::connect(addr);
+                let rx = listener.accept();
+                let (mut tx, (mut rx, _)) = futures_util::try_join!(tx, rx).unwrap();
+                tx.write_all(PACKET).await.0.unwrap();
+                let buffer = Vec::with_capacity(PACKET_LEN);
+                _ = rx.read_exact(buffer).await.unwrap();
+            };
+
+            let handles = (0..TASK_COUNT)
+                .map(|_| compio::runtime::spawn(task(listener.clone())))
+                .collect::<Vec<_>>();
+
+            for handle in handles {
+                handle.await;
+            }
+
+            Arc::into_inner(listener).unwrap().close().await.unwrap();
+        })
+    });
+
+    #[cfg(unix)]
+    group.bench_function("monoio", |b| {
+        let mut runtime = monoio::RuntimeBuilder::<monoio::FusionDriver>::new()
+            .enable_all()
+            .build()
+            .unwrap();
+        b.iter(|| {
+            runtime.block_on(async {
+                use monoio::io::{AsyncReadRentExt, AsyncWriteRentExt};
+
+                let listener = Arc::new(monoio::net::TcpListener::bind("127.0.0.1:0").unwrap());
+
+                let task = |listener: Arc<monoio::net::TcpListener>| async move {
+                    let addr = listener.local_addr().unwrap();
+                    let tx = monoio::net::TcpStream::connect(addr);
+                    let rx = listener.accept();
+                    let (mut tx, (mut rx, _)) = futures_util::try_join!(tx, rx).unwrap();
+                    tx.write_all(PACKET).await.0.unwrap();
+                    let buffer = Vec::with_capacity(PACKET_LEN);
+                    _ = rx.read_exact(buffer).await;
+                };
+
+                let handles = (0..TASK_COUNT)
+                    .map(|_| monoio::spawn(task(listener.clone())))
+                    .collect::<Vec<_>>();
+
+                for handle in handles {
+                    handle.await;
+                }
+
+                Arc::into_inner(listener).unwrap();
+            })
+        })
+    });
+
+    group.finish();
+}
+
+fn multi_udp(c: &mut Criterion) {
+    const TASK_COUNT: u32 = 100;
+    const PACKET_LEN: usize = 1024;
+    static PACKET: &[u8] = &[1u8; PACKET_LEN];
+
+    let mut group = c.benchmark_group("multi_udp");
+
+    // The socket may be dropped by firewall when the number is too large.
+    #[cfg(target_os = "linux")]
+    group
+        .sample_size(16)
+        .measurement_time(std::time::Duration::from_millis(2))
+        .warm_up_time(std::time::Duration::from_millis(2));
+
+    group.bench_function("tokio", |b| {
+        let runtime = tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+        b.to_async(&runtime).iter(|| async {
+            let task = || async {
+                let rx = tokio::net::UdpSocket::bind("127.0.0.1:0").await.unwrap();
+                let addr_rx = rx.local_addr().unwrap();
+                let tx = tokio::net::UdpSocket::bind("127.0.0.1:0").await.unwrap();
+                let addr_tx = tx.local_addr().unwrap();
+
+                rx.connect(addr_tx).await.unwrap();
+                tx.connect(addr_rx).await.unwrap();
+
+                {
+                    let mut pos = 0;
+                    while pos < PACKET_LEN {
+                        let res = tx.send(&PACKET[pos..]).await;
+                        pos += res.unwrap();
+                    }
+                }
+                {
+                    let mut buffer = vec![0; PACKET_LEN];
+                    let mut pos = 0;
+                    while pos < PACKET_LEN {
+                        let res = rx.recv(&mut buffer[pos..]).await;
+                        pos += res.unwrap();
+                    }
+                };
+            };
+
+            let handles = (0..TASK_COUNT)
+                .map(|_| tokio::spawn(task()))
+                .collect::<Vec<_>>();
+
+            for handle in handles {
+                handle.await.unwrap();
+            }
+        })
+    });
+
+    group.bench_function("compio", |b| {
+        let runtime = compio::runtime::Runtime::new().unwrap();
+        b.to_async(&runtime).iter(|| async {
+            let task = || async {
+                let rx = compio::net::UdpSocket::bind("127.0.0.1:0").await.unwrap();
+                let addr_rx = rx.local_addr().unwrap();
+                let tx = compio::net::UdpSocket::bind("127.0.0.1:0").await.unwrap();
+                let addr_tx = tx.local_addr().unwrap();
+
+                rx.connect(addr_tx).await.unwrap();
+                tx.connect(addr_rx).await.unwrap();
+
+                {
+                    let mut pos = 0;
+                    while pos < PACKET_LEN {
+                        let (res, _) = tx.send(&PACKET[pos..]).await.unwrap();
+                        pos += res;
+                    }
+                }
+                {
+                    let mut buffer = Vec::with_capacity(PACKET_LEN);
+                    while buffer.len() < PACKET_LEN {
+                        (_, buffer) = rx.recv(buffer).await.unwrap();
+                    }
+                };
+            };
+
+            let handles = (0..TASK_COUNT)
+                .map(|_| compio::runtime::spawn(task()))
+                .collect::<Vec<_>>();
+
+            for handle in handles {
+                handle.await;
+            }
+        })
+    });
+
+    #[cfg(unix)]
+    group.bench_function("monoio", |b| {
+        let mut runtime = monoio::RuntimeBuilder::<monoio::FusionDriver>::new()
+            .enable_all()
+            .build()
+            .unwrap();
+        b.iter(|| {
+            runtime.block_on(async {
+                let task = || async {
+                    let rx = monoio::net::udp::UdpSocket::bind("127.0.0.1:0").unwrap();
+                    let addr_rx = rx.local_addr().unwrap();
+                    let tx = monoio::net::udp::UdpSocket::bind("127.0.0.1:0").unwrap();
+                    let addr_tx = tx.local_addr().unwrap();
+
+                    rx.connect(addr_tx).await.unwrap();
+                    tx.connect(addr_rx).await.unwrap();
+
+                    {
+                        let mut pos = 0;
+                        while pos < PACKET_LEN {
+                            let res = tx.send(&PACKET[pos..]).await.0.unwrap();
+                            pos += res;
+                        }
+                    }
+                    {
+                        let mut buffer = Vec::with_capacity(PACKET_LEN);
+                        while buffer.len() < PACKET_LEN {
+                            buffer = rx.recv(buffer).await.1;
+                        }
+                    };
+                };
+
+                let handles = (0..TASK_COUNT)
+                    .map(|_| monoio::spawn(task()))
+                    .collect::<Vec<_>>();
+
+                for handle in handles {
+                    handle.await;
                 }
             })
         })
