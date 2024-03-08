bb63906fccc05df80f95aec5df11522a9ed9d284
王宇逸 <Strawberry_Str@hotmail.com>
2024-03-09 01:23:40 +0800

    fix(process,linux): remove the wrong usage of OwnedFd

--- compio-process/src/linux.rs
+++ compio-process/src/linux.rs
@@ -1,19 +1,21 @@
-use std::{io, os::fd::OwnedFd, process};
+use std::{io, process};
 
-use compio_driver::{op::PollRead, AsRawFd, FromRawFd};
+use compio_driver::op::PollRead;
 use compio_runtime::Runtime;
 
 pub async fn child_wait(mut child: process::Child) -> io::Result<process::ExitStatus> {
     #[cfg(feature = "linux_pidfd")]
     let fd = {
         use std::os::linux::process::ChildExt;
+
+        use compio_driver::AsRawFd;
+
         child.pidfd().ok().map(|fd| fd.as_raw_fd())
     };
     #[cfg(not(feature = "linux_pidfd"))]
-    let fd: Option<RawFd> = None;
+    let fd = None;
     if let Some(fd) = fd {
-        let fd = unsafe { OwnedFd::from_raw_fd(fd) };
-        let op = PollRead::new(fd.as_raw_fd());
+        let op = PollRead::new(fd);
         Runtime::current().submit(op).await.0?;
         child.wait()
     } else {
