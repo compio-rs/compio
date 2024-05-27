3fe0e84458186f0d7a12c79504d882a97cca8b0a
王宇逸 <Strawberry_Str@hotmail.com>
2024-05-28 01:42:07 +0800

    perf(net): add unix bench on unix

--- compio/benches/net.rs
+++ compio/benches/net.rs
@@ -223,6 +223,50 @@ fn echo_compio_win_named_pipe(b: &mut Bencher, content: Rc<[u8; BUFFER_SIZE]>) {
     })
 }
 
+#[cfg(unix)]
+fn echo_tokio_unix(b: &mut Bencher, content: &[u8; BUFFER_SIZE]) {
+    use tokio::{
+        io::{AsyncReadExt, AsyncWriteExt},
+        net::{UnixListener, UnixStream},
+    };
+
+    let runtime = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+    b.to_async(&runtime).iter_custom(|iter| async move {
+        let dir = tempfile::Builder::new()
+            .prefix("tokio-uds")
+            .tempdir()
+            .unwrap();
+        let sock_path = dir.path().join("connect.sock");
+        let listener = UnixListener::bind(&sock_path).unwrap();
+
+        let start = Instant::now();
+        for _i in 0..iter {
+            let (mut tx, (mut rx, _)) =
+                tokio::try_join!(UnixStream::connect(&sock_path), listener.accept()).unwrap();
+
+            let client = async move {
+                let mut buffer = [0u8; BUFFER_SIZE];
+                for _i in 0..BUFFER_COUNT {
+                    tx.write_all(content).await.unwrap();
+                    tx.read_exact(&mut buffer).await.unwrap();
+                }
+            };
+            let server = async move {
+                let mut buffer = [0u8; BUFFER_SIZE];
+                for _i in 0..BUFFER_COUNT {
+                    rx.read_exact(&mut buffer).await.unwrap();
+                    rx.write_all(&buffer).await.unwrap();
+                }
+            };
+            tokio::join!(client, server);
+        }
+        start.elapsed()
+    })
+}
+
 fn echo_compio_unix(b: &mut Bencher, content: Rc<[u8; BUFFER_SIZE]>) {
     use compio::{
         io::{AsyncReadExt, AsyncWriteExt},
@@ -268,6 +312,58 @@ fn echo_compio_unix(b: &mut Bencher, content: Rc<[u8; BUFFER_SIZE]>) {
     })
 }
 
+#[cfg(target_os = "linux")]
+fn echo_monoio_unix(b: &mut Bencher, content: &[u8; BUFFER_SIZE]) {
+    use monoio::{
+        io::{AsyncReadRentExt, AsyncWriteRentExt},
+        net::{UnixListener, UnixStream},
+    };
+
+    let runtime = MonoioRuntime::new();
+    b.to_async(&runtime).iter_custom(|iter| {
+        let content = Box::new(*content);
+        async move {
+            let dir = tempfile::Builder::new()
+                .prefix("monoio-uds")
+                .tempdir()
+                .unwrap();
+            let sock_path = dir.path().join("connect.sock");
+            let listener = UnixListener::bind(&sock_path).unwrap();
+
+            let start = Instant::now();
+            for _i in 0..iter {
+                let (mut tx, (mut rx, _)) =
+                    futures_util::try_join!(UnixStream::connect(&sock_path), listener.accept())
+                        .unwrap();
+
+                let client = async {
+                    let mut content = content.clone();
+                    let mut buffer = Box::new([0u8; BUFFER_SIZE]);
+                    let mut res;
+                    for _i in 0..BUFFER_COUNT {
+                        (res, content) = tx.write_all(content).await;
+                        res.unwrap();
+                        (res, buffer) = tx.read_exact(buffer).await;
+                        res.unwrap();
+                    }
+                };
+                let server = async move {
+                    let mut buffer = Box::new([0u8; BUFFER_SIZE]);
+                    let mut res;
+                    for _i in 0..BUFFER_COUNT {
+                        (res, buffer) = rx.read_exact(buffer).await;
+                        res.unwrap();
+                        (res, buffer) = rx.write_all(buffer).await;
+                        res.unwrap();
+                    }
+                };
+                futures_util::join!(client, server);
+            }
+            start.elapsed()
+        }
+    })
+}
+
 fn echo(c: &mut Criterion) {
     let mut rng = thread_rng();
 
@@ -290,7 +386,11 @@ fn echo(c: &mut Criterion) {
         echo_compio_win_named_pipe(b, content.clone())
     });
 
+    #[cfg(unix)]
+    group.bench_function("tokio-unix", |b| echo_tokio_unix(b, &content));
     group.bench_function("compio-unix", |b| echo_compio_unix(b, content.clone()));
+    #[cfg(target_os = "linux")]
+    group.bench_function("monoio-unix", |b| echo_monoio_unix(b, &content));
 
     group.finish();
 }
