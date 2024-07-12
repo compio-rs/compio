439ff9c9c6decbddc58f9e582743dcb1d5357b13
Sherlock Holo <sherlockya@gmail.com>
2024-07-12 14:18:59 +0800

    remove async fd read buffer pool test
    a file behind async fd may failed because not support epoll

--- compio-fs/tests/buffer_pool.rs
+++ compio-fs/tests/buffer_pool.rs
@@ -1,10 +1,8 @@
 use std::io::Write;
-#[cfg(unix)]
-use std::io::{Seek, SeekFrom};
 
-use compio_fs::File;
 #[cfg(unix)]
-use compio_fs::{pipe, AsyncFd};
+use compio_fs::pipe;
+use compio_fs::File;
 use compio_io::AsyncReadAtBufferPool;
 #[cfg(unix)]
 use compio_io::{AsyncReadBufferPool, AsyncWriteExt};
@@ -25,21 +23,6 @@ async fn test_read_file() {
     let file = File::open(tempfile.path()).await.unwrap();
     let buffer_pool = BufferPool::new(1, 15).unwrap();
     let buf = file.read_at_buffer_pool(&buffer_pool, 0, 0).await.unwrap();
-
-    assert_eq!(buf.len(), HELLO.len());
-    assert_eq!(buf.as_ref(), HELLO);
-}
-
-#[cfg(unix)]
-#[compio_macros::test]
-async fn test_read_async_fd() {
-    let mut tempfile = tempfile();
-    tempfile.write_all(HELLO).unwrap();
-    tempfile.seek(SeekFrom::Start(0)).unwrap();
-
-    let mut file = AsyncFd::new(tempfile).unwrap();
-    let buffer_pool = BufferPool::new(1, 15).unwrap();
-    let buf = file.read_buffer_pool(&buffer_pool, 0).await.unwrap();
 
     assert_eq!(buf.len(), HELLO.len());
     assert_eq!(buf.as_ref(), HELLO);
