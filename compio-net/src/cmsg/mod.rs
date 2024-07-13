840f2379c0785b2ae778a8c4707a5f7a80a47131
AsakuraMizu <asakuramizu111@gmail.com>
2024-07-14 02:19:00 +0800

    feat(net): helpers to handle control messages

---
+++ compio-net/src/cmsg/mod.rs
@@ -0,0 +1,115 @@
+use std::marker::PhantomData;
+
+use compio_buf::{IoBuf, IoBufMut};
+
+cfg_if::cfg_if! {
+    if #[cfg(windows)] {
+        #[path = "windows.rs"]
+        mod sys;
+    } else if #[cfg(unix)] {
+        #[path = "unix.rs"]
+        mod sys;
+    }
+}
+
+pub use sys::CMsgRef;
+
+/// An iterator for control messages.
+pub struct CMsgIter<'a> {
+    inner: sys::CMsgIter,
+    _p: PhantomData<&'a ()>,
+}
+
+impl<'a> CMsgIter<'a> {
+    /// Create [`CMsgIter`] with the given buffer.
+    ///
+    /// # Panics
+    ///
+    /// This function will panic if the buffer is too short or not properly
+    /// aligned.
+    ///
+    /// # Safety
+    ///
+    /// The buffer should contain valid control messages.
+    pub unsafe fn new<B: IoBuf>(buffer: &'a B) -> Self {
+        Self {
+            inner: sys::CMsgIter::new(buffer.as_buf_ptr(), buffer.buf_len()),
+            _p: PhantomData,
+        }
+    }
+}
+
+impl<'a> Iterator for CMsgIter<'a> {
+    type Item = CMsgRef<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        unsafe {
+            let cmsg = self.inner.current();
+            self.inner.next();
+            cmsg
+        }
+    }
+}
+
+/// Helper to construct control message.
+pub struct CMsgBuilder<B> {
+    inner: sys::CMsgIter,
+    buffer: B,
+    len: usize,
+}
+
+impl<B> CMsgBuilder<B> {
+    /// Finishes building, returns the buffer and the length of the control
+    /// message.
+    pub fn build(self) -> (B, usize) {
+        (self.buffer, self.len)
+    }
+
+    /// Try to append a control message entry into the buffer. If the buffer
+    /// does not have enough space or is not properly aligned with the value
+    /// type, returns `None`.
+    ///
+    /// # Safety
+    ///
+    /// TODO: This function may be safe? Given that the buffer is zeroed,
+    /// properly aligned and has enough space, safety conditions of all unsafe
+    /// functions involved are satisfied, except for `CMSG_*`/`wsa_cmsg_*`, as
+    /// their safety are not documented.
+    pub unsafe fn try_push<T>(
+        &mut self,
+        level: sys::c_int,
+        ty: sys::c_int,
+        value: T,
+    ) -> Option<()> {
+        if !self.inner.is_aligned::<T>() || !self.inner.is_space_enough::<T>() {
+            return None;
+        }
+
+        let mut cmsg = self.inner.current_mut()?;
+        cmsg.set_level(level);
+        cmsg.set_ty(ty);
+        cmsg.set_data(value);
+
+        self.inner.next();
+        self.len += sys::space_of::<T>();
+        Some(())
+    }
+}
+
+impl<B: IoBufMut> CMsgBuilder<B> {
+    /// Create [`CMsgBuilder`] with the given buffer. The buffer will be zeroed
+    /// on creation.
+    ///
+    /// # Panics
+    ///
+    /// This function will panic if the buffer is too short or not properly
+    /// aligned.
+    pub fn new(mut buffer: B) -> Self {
+        buffer.as_mut_slice().fill(std::mem::MaybeUninit::zeroed());
+        Self {
+            inner: sys::CMsgIter::new(buffer.as_buf_mut_ptr(), buffer.buf_len()),
+            buffer,
+            len: 0,
+        }
+    }
+}
---
+++ compio-net/src/cmsg/unix.rs
@@ -0,0 +1,106 @@
+use std::mem;
+
+pub use libc::c_int;
+use libc::{cmsghdr, msghdr, CMSG_DATA, CMSG_FIRSTHDR, CMSG_LEN, CMSG_NXTHDR, CMSG_SPACE};
+
+/// Reference to a control message.
+pub struct CMsgRef<'a>(&'a cmsghdr);
+
+impl<'a> CMsgRef<'a> {
+    /// Returns the level of the control message.
+    pub fn level(&self) -> c_int {
+        self.0.cmsg_level
+    }
+
+    /// Returns the type of the control message.
+    pub fn ty(&self) -> c_int {
+        self.0.cmsg_type
+    }
+
+    /// Returns the length of the control message.
+    #[allow(clippy::len_without_is_empty)]
+    pub fn len(&self) -> usize {
+        self.0.cmsg_len as _
+    }
+
+    /// Returns a reference to the data of the control message.
+    ///
+    /// # Safety
+    ///
+    /// The data part must be properly aligned and contains an initialized
+    /// instance of `T`.
+    pub unsafe fn data<T>(&self) -> &T {
+        let data_ptr = CMSG_DATA(self.0);
+        data_ptr.cast::<T>().as_ref().unwrap()
+    }
+}
+
+pub(crate) struct CMsgMut<'a>(&'a mut cmsghdr);
+
+impl<'a> CMsgMut<'a> {
+    pub(crate) fn set_level(&mut self, level: c_int) {
+        self.0.cmsg_level = level;
+    }
+
+    pub(crate) fn set_ty(&mut self, ty: c_int) {
+        self.0.cmsg_type = ty;
+    }
+
+    pub(crate) unsafe fn set_data<T>(&mut self, data: T) {
+        self.0.cmsg_len = CMSG_LEN(mem::size_of::<T>() as _) as _;
+        let data_ptr = CMSG_DATA(self.0);
+        std::ptr::write(data_ptr.cast::<T>(), data);
+    }
+}
+
+pub(crate) struct CMsgIter {
+    msg: msghdr,
+    cmsg: *mut cmsghdr,
+}
+
+impl CMsgIter {
+    pub(crate) fn new(ptr: *const u8, len: usize) -> Self {
+        assert!(len >= unsafe { CMSG_SPACE(0) as _ }, "buffer too short");
+        assert!(ptr.cast::<cmsghdr>().is_aligned(), "misaligned buffer");
+
+        let mut msg: msghdr = unsafe { mem::zeroed() };
+        msg.msg_control = ptr as _;
+        msg.msg_controllen = len as _;
+        // SAFETY: msg is initialized and valid
+        let cmsg = unsafe { CMSG_FIRSTHDR(&msg) };
+        Self { msg, cmsg }
+    }
+
+    pub(crate) unsafe fn current<'a>(&self) -> Option<CMsgRef<'a>> {
+        self.cmsg.as_ref().map(CMsgRef)
+    }
+
+    pub(crate) unsafe fn next(&mut self) {
+        if !self.cmsg.is_null() {
+            self.cmsg = CMSG_NXTHDR(&self.msg, self.cmsg);
+        }
+    }
+
+    pub(crate) unsafe fn current_mut<'a>(&self) -> Option<CMsgMut<'a>> {
+        self.cmsg.as_mut().map(CMsgMut)
+    }
+
+    pub(crate) fn is_aligned<T>(&self) -> bool {
+        self.msg.msg_control.cast::<T>().is_aligned()
+    }
+
+    pub(crate) fn is_space_enough<T>(&self) -> bool {
+        if !self.cmsg.is_null() {
+            let space = unsafe { CMSG_SPACE(mem::size_of::<T>() as _) as usize };
+            #[allow(clippy::unnecessary_cast)]
+            let max = self.msg.msg_control as usize + self.msg.msg_controllen as usize;
+            self.cmsg as usize + space <= max
+        } else {
+            false
+        }
+    }
+}
+
+pub(crate) fn space_of<T>() -> usize {
+    unsafe { CMSG_SPACE(mem::size_of::<T>() as _) as _ }
+}
