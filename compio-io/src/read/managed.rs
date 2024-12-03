c739459d34f355e8f589969f4518521b73ba2403
王宇逸 <Strawberry_Str@hotmail.com>
2024-12-03 17:32:38 +0900

    feat(io): impl for Cursor

--- compio-io/src/read/managed.rs
+++ compio-io/src/read/managed.rs
@@ -1,4 +1,4 @@
-use std::ops::DerefMut;
+use std::{io::Cursor, ops::DerefMut};
 
 use crate::IoResult;
 
@@ -26,7 +26,7 @@ pub trait AsyncReadManaged {
 /// # AsyncReadAtManaged
 ///
 /// Async read with buffer pool and position
-pub trait AsyncReadAtManaged {
+pub trait AsyncReadManagedAt {
     /// Buffer pool type
     type BufferPool;
     /// Filled buffer type
@@ -39,8 +39,27 @@ pub trait AsyncReadAtManaged {
     /// if `len` > 0, `min(len, inner buffer size)` will be the read max len
     async fn read_managed_at<'a>(
         &self,
-        buffer_pool: &'a Self::BufferPool,
         pos: u64,
+        buffer_pool: &'a Self::BufferPool,
         len: usize,
     ) -> IoResult<Self::Buffer<'a>>;
+}
+
+impl<A: AsyncReadManagedAt> AsyncReadManaged for Cursor<A> {
+    type Buffer<'a> = A::Buffer<'a>;
+    type BufferPool = A::BufferPool;
+
+    async fn read_managed<'a>(
+        &mut self,
+        buffer_pool: &'a Self::BufferPool,
+        len: usize,
+    ) -> IoResult<Self::Buffer<'a>> {
+        let pos = self.position();
+        let buf = self
+            .get_ref()
+            .read_managed_at(pos, buffer_pool, len)
+            .await?;
+        self.set_position(pos + buf.len() as u64);
+        Ok(buf)
+    }
 }
