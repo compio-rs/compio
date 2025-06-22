9d853ff92fbdbb253715362b90cb1b7f1e95b35f
王宇逸 <Strawberry_Str@hotmail.com>
2025-06-22 13:46:30 +0800

    test with InMemoryPipe

--- compio-io/src/framed/codec/serde_json.rs
+++ compio-io/src/framed/codec/serde_json.rs
@@ -115,14 +115,18 @@ impl<T: DeserializeOwned> Decoder<T> for SerdeJsonCodec {
 
 #[cfg(test)]
 mod test {
-    use std::io::Cursor;
+    use std::{
+        io::{self, Cursor},
+        rc::Rc,
+    };
 
-    use futures_util::{SinkExt, StreamExt};
+    use compio_buf::{BufResult, IoBuf, IoBufMut};
+    use futures_util::{SinkExt, StreamExt, lock::Mutex};
     use serde::{Deserialize, Serialize};
 
     use crate::{
+        AsyncRead, AsyncReadAt, AsyncWrite, AsyncWriteAt,
         framed::{Framed, codec::serde_json::SerdeJsonCodec, frame::LengthDelimited},
-        split,
     };
 
     #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
@@ -131,11 +135,61 @@ mod test {
         bar: usize,
     }
 
+    struct InMemoryPipe(Cursor<Rc<Mutex<Vec<u8>>>>);
+
+    impl AsyncRead for InMemoryPipe {
+        async fn read<B: IoBufMut>(&mut self, buf: B) -> BufResult<usize, B> {
+            let BufResult(res, buf) = self
+                .0
+                .get_ref()
+                .lock()
+                .await
+                .read_at(buf, self.0.position())
+                .await;
+            match res {
+                Ok(len) => {
+                    self.0.set_position(self.0.position() + len as u64);
+                    BufResult(Ok(len), buf)
+                }
+                Err(_) => BufResult(res, buf),
+            }
+        }
+    }
+
+    impl AsyncWrite for InMemoryPipe {
+        async fn write<T: IoBuf>(&mut self, buf: T) -> BufResult<usize, T> {
+            let BufResult(res, buf) = self
+                .0
+                .get_ref()
+                .lock()
+                .await
+                .write_at(buf, self.0.position())
+                .await;
+            match res {
+                Ok(len) => {
+                    self.0.set_position(self.0.position() + len as u64);
+                    BufResult(Ok(len), buf)
+                }
+                Err(_) => BufResult(res, buf),
+            }
+        }
+
+        async fn flush(&mut self) -> io::Result<()> {
+            self.0.get_ref().lock().await.flush().await
+        }
+
+        async fn shutdown(&mut self) -> io::Result<()> {
+            self.0.get_ref().lock().await.shutdown().await
+        }
+    }
+
     #[compio_macros::test]
     async fn test_framed() {
         let codec = SerdeJsonCodec::new();
         let framer = LengthDelimited::new();
-        let (r, w) = split(Cursor::new(Vec::new()));
+        let buf = Rc::new(Mutex::new(vec![]));
+        let r = InMemoryPipe(Cursor::new(buf.clone()));
+        let w = InMemoryPipe(Cursor::new(buf));
         let mut framed = Framed::symmetric::<Test>(codec, framer)
             .with_reader(r)
             .with_writer(w);
