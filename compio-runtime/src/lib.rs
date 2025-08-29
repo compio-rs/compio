793888f040e7ec8e13336078af5b1300e467656b
Pop <gm@miao.dev>
2025-08-29 08:08:37 -0400

    fix: TimerFuture being Send and Sync (#470)
    * fix: TimerFuture being Send and Sync
    
    * fix: ci

--- compio-runtime/src/lib.rs
+++ compio-runtime/src/lib.rs
@@ -27,3 +27,27 @@ pub use runtime::{
     BorrowedBuffer, BufferPool, JoinHandle, Runtime, RuntimeBuilder, spawn, spawn_blocking, submit,
     submit_with_flags,
 };
+
+/// Macro that asserts a type *DOES NOT* implement some trait. Shamelessly
+/// copied from <https://users.rust-lang.org/t/a-macro-to-assert-that-a-type-does-not-implement-trait-bounds/31179>.
+///
+/// # Example
+///
+/// ```rust,ignore
+/// assert_not_impl!(u8, From<u16>);
+/// ```
+macro_rules! assert_not_impl {
+    ($x:ty, $($t:path),+ $(,)*) => {
+        const _: fn() -> () = || {
+            struct Check<T: ?Sized>(T);
+            trait AmbiguousIfImpl<A> { fn some_item() { } }
+
+            impl<T: ?Sized> AmbiguousIfImpl<()> for Check<T> { }
+            impl<T: ?Sized $(+ $t)*> AmbiguousIfImpl<u8> for Check<T> { }
+
+            <Check::<$x> as AmbiguousIfImpl<_>>::some_item()
+        };
+    };
+}
+
+use assert_not_impl;
