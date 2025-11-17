9a90c84431e9b8e3bf6cc2f41ce24e7dedfe7314
王宇逸 <Strawberry_Str@hotmail.com>
2025-11-18 00:36:46 +0800

    fix(ws): adjust examples & tests

--- compio-ws/examples/autobahn-server.rs
+++ compio-ws/examples/autobahn-server.rs
@@ -1,20 +1,24 @@
+#![allow(clippy::collapsible_match)]
+
 use std::net::SocketAddr;
 
+use compio_log::*;
 use compio_net::{TcpListener, TcpStream};
 use compio_ws::{WebSocketConfig, accept_async_with_config};
-use log::*;
 use tungstenite::{Error, Result};
 
 async fn accept_connection(peer: SocketAddr, stream: TcpStream) {
     if let Err(e) = handle_connection(peer, stream).await {
         match e {
             Error::ConnectionClosed | Error::Protocol(_) | Error::Utf8(_) => (),
-            err => error!("Error processing connection: {err}"),
+            _err => {
+                error!("Error processing connection: {_err}");
+            }
         }
     }
 }
 
-async fn handle_connection(peer: SocketAddr, stream: TcpStream) -> Result<()> {
+async fn handle_connection(_peer: SocketAddr, stream: TcpStream) -> Result<()> {
     let mut config = WebSocketConfig::default();
     config.max_message_size = Some(64 * 1024 * 1024);
     config.max_frame_size = Some(16 * 1024 * 1024);
@@ -23,7 +27,7 @@ async fn handle_connection(peer: SocketAddr, stream: TcpStream) -> Result<()> {
         .await
         .expect("Failed to accept");
 
-    info!("New WebSocket connection: {peer}");
+    info!("New WebSocket connection: {_peer}");
 
     loop {
         match ws_stream.read().await {
@@ -34,7 +38,7 @@ async fn handle_connection(peer: SocketAddr, stream: TcpStream) -> Result<()> {
             }
             Err(e) => match e {
                 Error::ConnectionClosed => {
-                    info!("Connection closed normally: {peer}");
+                    info!("Connection closed normally: {_peer}");
                     break;
                 }
                 _ => {
@@ -50,7 +54,9 @@ async fn handle_connection(peer: SocketAddr, stream: TcpStream) -> Result<()> {
 
 #[compio_macros::main]
 async fn main() {
-    env_logger::init();
+    tracing_subscriber::fmt()
+        .with_max_level(Level::TRACE)
+        .init();
 
     let addr = "127.0.0.1:9002";
     let listener = TcpListener::bind(addr).await.expect("Can't listen");
@@ -62,8 +68,8 @@ async fn main() {
                 info!("Peer address: {addr}");
                 compio_runtime::spawn(accept_connection(addr, stream)).detach();
             }
-            Err(e) => {
-                error!("Error accepting connection: {e}");
+            Err(_e) => {
+                error!("Error accepting connection: {_e}");
             }
         }
     }
