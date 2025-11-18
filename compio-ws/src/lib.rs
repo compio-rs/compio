b9d10163980990eee9cee5ce888c061e88f061c8
Krishna Vishal <krishnavishal2008@gmail.com>
2025-11-18 21:41:46 +0530

    fix(ws): initialize `SyncStream` with 128 KB capacity to match tungstenite's default size.

--- compio-ws/src/lib.rs
+++ compio-ws/src/lib.rs
@@ -186,7 +186,7 @@ where
     S: AsyncRead + AsyncWrite,
     C: Callback,
 {
-    let sync_stream = SyncStream::new(stream);
+    let sync_stream = SyncStream::with_capacity(128 * 1024, stream);
     let mut handshake_result = tungstenite::accept_hdr_with_config(sync_stream, callback, config);
 
     loop {
@@ -250,7 +250,7 @@ where
     R: IntoClientRequest,
     S: AsyncRead + AsyncWrite,
 {
-    let sync_stream = SyncStream::new(stream);
+    let sync_stream = SyncStream::with_capacity(128 * 1024, stream);
     let mut handshake_result =
         tungstenite::client::client_with_config(request, sync_stream, config);
 
