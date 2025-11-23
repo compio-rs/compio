c9838ad0566993313208457e09cb9ecd76e7513c
王宇逸 <Strawberry_Str@hotmail.com>
2025-11-24 00:02:26 +0800

    fix(runtime): notify-always for OptWaker

--- compio-runtime/src/runtime/opt_waker.rs
+++ compio-runtime/src/runtime/opt_waker.rs
@@ -6,11 +6,13 @@ use std::{
     task::{Wake, Waker},
 };
 
+#[cfg(not(feature = "notify-always"))]
 use crate::runtime::send_wrapper::SendWrapper;
 
 /// An optimized waker that avoids unnecessary wake-ups on the same thread.
 pub struct OptWaker {
     waker: Waker,
+    #[cfg(not(feature = "notify-always"))]
     current_thread: SendWrapper<()>,
     is_woke: AtomicBool,
 }
@@ -19,6 +21,7 @@ impl OptWaker {
     pub(crate) fn new(waker: Waker) -> Arc<Self> {
         Arc::new(Self {
             waker,
+            #[cfg(not(feature = "notify-always"))]
             current_thread: SendWrapper::new(()),
             is_woke: AtomicBool::new(false),
         })
@@ -29,6 +32,18 @@ impl OptWaker {
     pub fn reset(&self) -> bool {
         self.is_woke.swap(false, Ordering::AcqRel)
     }
+
+    #[inline]
+    fn should_wake(&self) -> bool {
+        #[cfg(feature = "notify-always")]
+        {
+            true
+        }
+        #[cfg(not(feature = "notify-always"))]
+        {
+            !self.current_thread.valid()
+        }
+    }
 }
 
 impl Wake for OptWaker {
@@ -37,7 +52,7 @@ impl Wake for OptWaker {
     }
 
     fn wake_by_ref(self: &Arc<Self>) {
-        if !self.is_woke.swap(true, Ordering::AcqRel) && !self.current_thread.valid() {
+        if !self.is_woke.swap(true, Ordering::AcqRel) && self.should_wake() {
             self.waker.wake_by_ref();
         }
     }
