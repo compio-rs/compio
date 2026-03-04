d88235b141de2d874b220f24976cac230b2f7e9f
Yuyi Wang <Strawberry_Str@hotmail.com>
2026-03-05 00:56:10 +0800

    feat(io): traits for async read multi

---
+++ compio-io/src/read/multi.rs
@@ -0,0 +1,40 @@
+use futures_util::Stream;
+
+use crate::{AsyncReadManaged, AsyncReadManagedAt, IoResult};
+
+/// # AsyncReadMulti
+///
+/// Async read with buffer pool and returns a stream of managed buffers.
+pub trait AsyncReadMulti: AsyncReadManaged {
+    /// Read some bytes from this source with [`AsyncReadManaged::BufferPool`]
+    /// and return a stream of [`AsyncReadManaged::Buffer`].
+    ///
+    /// If `len` == 0, will use [`AsyncReadManaged::BufferPool`] inner buffer
+    /// size as the max len, if `len` > 0, `min(len, inner buffer size)` will be
+    /// the read max len.
+    fn read_multi<'a>(
+        &mut self,
+        buffer_pool: &'a Self::BufferPool,
+        len: usize,
+    ) -> impl Stream<Item = IoResult<Self::Buffer<'a>>>;
+}
+
+/// # AsyncReadMultiAt
+///
+/// Async read with buffer pool and position, returns a stream of managed
+/// buffers.
+pub trait AsyncReadMultiAt: AsyncReadManagedAt {
+    /// Read some bytes from this source at position with
+    /// [`AsyncReadManagedAt::BufferPool`] and return a stream of
+    /// [`AsyncReadManagedAt::Buffer`].
+    ///
+    /// If `len` == 0, will use [`AsyncReadManagedAt::BufferPool`] inner buffer
+    /// size as the max len, if `len` > 0, `min(len, inner buffer size)` will be
+    /// the read max len.
+    fn read_multi_at<'a>(
+        &self,
+        buffer_pool: &'a Self::BufferPool,
+        len: usize,
+        pos: u64,
+    ) -> impl Stream<Item = IoResult<Self::Buffer<'a>>>;
+}
