74e3c4bc8c993f806d2340db318f2a5a94494bd2
Yuyi Wang <Strawberry_Str@hotmail.com>
2026-03-11 14:48:28 +0800

    feat(net): incoming

--- compio-net/src/socket.rs
+++ compio-net/src/socket.rs
@@ -19,6 +19,7 @@ use compio_driver::{
 };
 use compio_runtime::{Attacher, BorrowedBuffer, BufferPool, fd::PollFd};
 use futures_util::StreamExt;
+pub(crate) use incoming::Incoming;
 use socket2::{Domain, Protocol, SockAddr, Socket as Socket2, Type};
 
 #[derive(Debug, Clone)]
@@ -119,6 +120,10 @@ impl Socket {
         op.update_context()?;
         let (accept_sock, addr) = op.into_addr()?;
         Ok((Self::from_socket2(accept_sock)?, addr))
+    }
+
+    pub fn incoming(&self) -> Incoming<'_> {
+        Incoming::new(self)
     }
 
     pub fn close(self) -> impl Future<Output = io::Result<()>> {
@@ -465,4 +470,73 @@ async fn submit_zerocopy<T: OpCode + IntoInner + 'static>(
     };
 
     BufResult(res, fut)
+}
+
+#[cfg(unix)]
+mod incoming {
+    use std::{
+        io,
+        os::fd::FromRawFd,
+        pin::Pin,
+        task::{Context, Poll, ready},
+    };
+
+    use compio_buf::{BufResult, IntoInner};
+    use compio_driver::{SharedFd, ToSharedFd, op::AcceptMulti};
+    use compio_runtime::SubmitMulti;
+    use futures_util::{Stream, StreamExt, stream::FusedStream};
+    use socket2::Socket as Socket2;
+
+    use crate::Socket;
+
+    pub struct Incoming<'a> {
+        listener: &'a Socket,
+        op: Option<SubmitMulti<AcceptMulti<SharedFd<Socket2>>>>,
+    }
+
+    impl<'a> Incoming<'a> {
+        pub fn new(listener: &'a Socket) -> Self {
+            Self { listener, op: None }
+        }
+
+        fn create_op(listener: &'a Socket) -> SubmitMulti<AcceptMulti<SharedFd<Socket2>>> {
+            compio_runtime::submit_multi(AcceptMulti::new(listener.to_shared_fd()))
+        }
+    }
+
+    impl Stream for Incoming<'_> {
+        type Item = io::Result<Socket>;
+
+        fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+            let this = self.get_mut();
+            loop {
+                if let Some(op) = &mut this.op {
+                    let res = ready!(op.poll_next_unpin(cx));
+                    if let Some(BufResult(res, _)) = res {
+                        let socket = if op.is_terminated() && res.is_ok() {
+                            let old_op = std::mem::replace(op, Self::create_op(this.listener));
+                            old_op
+                                .try_take()
+                                .map_err(|_| ())
+                                .expect("AcceptMulti has not completed")
+                                .into_inner()
+                        } else {
+                            unsafe { Socket2::from_raw_fd(res? as _) }
+                        };
+                        return Poll::Ready(Some(Socket::from_socket2(socket)));
+                    } else {
+                        this.op = None;
+                    }
+                } else {
+                    this.op = Some(Self::create_op(this.listener));
+                }
+            }
+        }
+    }
+
+    impl FusedStream for Incoming<'_> {
+        fn is_terminated(&self) -> bool {
+            false
+        }
+    }
 }
