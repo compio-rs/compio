aabe62e59804144eefb9324a0fbdb7dca12fe1ac
Fantix King <fantix.king@gmail.com>
2026-03-19 11:08:44 -0400

    fix(io): enable bytemuck:min_const_generics

--- compio-io/src/ancillary/bytemuck_ext.rs
+++ compio-io/src/ancillary/bytemuck_ext.rs
@@ -71,15 +71,4 @@ impl_bytemuck_marker!(
     f64,
 );
 
-macro_rules! impl_bytemuck_marker_for_array {
-    ($($N:expr),* $(,)?) => {
-        $(
-            impl<T> BitwiseAncillaryData for [T; $N] where T: BitwiseAncillaryData {}
-        )*
-    };
-}
-
-impl_bytemuck_marker_for_array!(
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
-    27, 28, 29, 30, 31, 32, 48, 64, 96, 128, 256, 512,
-);
+impl<T: BitwiseAncillaryData, const N: usize> BitwiseAncillaryData for [T; N] {}
