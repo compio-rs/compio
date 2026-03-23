b39fd0ee14c1d3add8816da1ae7e54cc672c7fc6
Sherlock Holo <sherlockya@gmail.com>
2026-03-23 17:39:55 +0800

    feat: export more error types

--- compio-quic/src/lib.rs
+++ compio-quic/src/lib.rs
@@ -36,7 +36,7 @@ mod socket;
 
 #[cfg(rustls)]
 pub use builder::{ClientBuilder, ServerBuilder};
-pub use connection::{Connecting, Connection, ConnectionError};
+pub use connection::{Connecting, Connection, ConnectionError, OpenStreamError, SendDatagramError};
 pub use endpoint::{Endpoint, EndpointStats};
 pub use incoming::{Incoming, IncomingFuture};
 pub use recv_stream::{ReadError, ReadExactError, RecvStream, ResetError};
