b0dfe2c8ab9dcef85df53b8fb393fcd28ba091e1
Yuyi Wang <Strawberry_Str@hotmail.com>
2026-04-02 17:27:26 +0800

    docs(io): fix broken links

--- compio-io/src/read/multi.rs
+++ compio-io/src/read/multi.rs
@@ -6,12 +6,14 @@ use crate::{AsyncReadManaged, AsyncReadManagedAt, IoResult};
 ///
 /// Async read with buffer pool and returns a stream of managed buffers.
 pub trait AsyncReadMulti: AsyncReadManaged {
-    /// Read some bytes from this source with [`AsyncReadManaged::BufferPool`]
-    /// and return a stream of [`AsyncReadManaged::Buffer`].
+    /// Read some bytes from this source and return a stream of
+    /// [`AsyncReadManaged::Buffer`].
     ///
-    /// If `len` == 0, will use [`AsyncReadManaged::BufferPool`] inner buffer
-    /// size as the max len, if `len` > 0, `min(len, inner buffer size)` will be
-    /// the read max len.
+    /// # Implementation Note
+    ///
+    /// - If `len` == 0, implementation should use buffer's size as `len`
+    /// - if `len` > 0, `min(len, buffer_size)` will be the max number of bytes
+    ///   to be read.
     fn read_multi(&mut self, len: usize) -> impl Stream<Item = IoResult<Self::Buffer>>;
 }
 
@@ -20,12 +22,13 @@ pub trait AsyncReadMulti: AsyncReadManaged {
 /// Async read with buffer pool and position, returns a stream of managed
 /// buffers.
 pub trait AsyncReadMultiAt: AsyncReadManagedAt {
-    /// Read some bytes from this source at position with
-    /// [`AsyncReadManagedAt::BufferPool`] and return a stream of
+    /// Read some bytes from this source at position and return a stream of
     /// [`AsyncReadManagedAt::Buffer`].
     ///
-    /// If `len` == 0, will use [`AsyncReadManagedAt::BufferPool`] inner buffer
-    /// size as the max len, if `len` > 0, `min(len, inner buffer size)` will be
-    /// the read max len.
+    /// # Implementation Note
+    ///
+    /// - If `len` == 0, implementation should use buffer's size as `len`
+    /// - if `len` > 0, `min(len, buffer_size)` will be the max number of bytes
+    ///   to be read.
     fn read_multi_at(&self, len: usize, pos: u64) -> impl Stream<Item = IoResult<Self::Buffer>>;
 }
