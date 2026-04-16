d79ecc934fced55c3b26af3abd27c5a1da19d606
Yuyi Wang <Strawberry_Str@hotmail.com>
2026-04-17 03:11:05 +0800

    refactor(io): simplify AsyncStream

--- compio-io/src/compat/async_stream.rs
+++ compio-io/src/compat/async_stream.rs
@@ -20,18 +20,9 @@ pin_project! {
     /// A stream wrapper for [`futures_util::io`] traits.
     pub struct AsyncStream<S: Splittable> {
         #[pin]
-        read_inner: SyncStreamReadHalf<S::ReadHalf>,
+        read_inner: AsyncReadStream<S::ReadHalf>,
         #[pin]
-        write_inner: SyncStreamWriteHalf<S::WriteHalf>,
-        read_future: Option<PinBoxFuture<io::Result<usize>>>,
-        write_future: Option<PinBoxFuture<io::Result<usize>>>,
-        shutdown_future: Option<PinBoxFuture<io::Result<()>>>,
-        read_waker: Option<Waker>,
-        read_uninit_waker: Option<Waker>,
-        read_buf_waker: Option<Waker>,
-        write_waker: Option<Waker>,
-        flush_waker: Option<Waker>,
-        close_waker: Option<Waker>,
+        write_inner: AsyncWriteStream<S::WriteHalf>,
         #[pin]
         _p: PhantomPinned,
     }
@@ -51,17 +42,8 @@ impl<S: Splittable> AsyncStream<S> {
     fn new_impl(inner: SyncStream<S>) -> Self {
         let (read_inner, write_inner) = inner.split();
         Self {
-            read_inner,
-            write_inner,
-            read_future: None,
-            write_future: None,
-            shutdown_future: None,
-            read_waker: None,
-            read_uninit_waker: None,
-            read_buf_waker: None,
-            write_waker: None,
-            flush_waker: None,
-            close_waker: None,
+            read_inner: AsyncReadStream::new_impl(read_inner),
+            write_inner: AsyncWriteStream::new_impl(write_inner),
             _p: PhantomPinned,
         }
     }
@@ -255,49 +237,16 @@ fn replace_waker(waker_slot: &mut Option<Waker>, waker: &Waker) {
     }
 }
 
-impl<S: Splittable + 'static> AsyncStream<S>
-where
-    S::ReadHalf: AsyncRead + Unpin,
-{
-    fn poll_read_impl(self: Pin<&mut Self>) -> Poll<io::Result<usize>> {
-        let this = self.project();
-        // SAFETY:
-        // - The future won't live longer than the stream.
-        // - The stream is internally mutable.
-        // - The future only accesses the corresponding buffer and fields.
-        //   - No access overlap between the futures.
-        let inner = unsafe { extend_lifetime_mut(this.read_inner.get_mut()) };
-        let arr = WakerArray([
-            this.read_waker.as_ref().cloned(),
-            this.read_uninit_waker.as_ref().cloned(),
-            this.read_buf_waker.as_ref().cloned(),
-        ]);
-        let waker = Waker::from(Arc::new(arr));
-        let cx = &mut Context::from_waker(&waker);
-        let res = poll_future!(this.read_future, cx, inner.fill_read_buf());
-        Poll::Ready(res)
-    }
-}
-
 impl<S: Splittable + 'static> futures_util::AsyncRead for AsyncStream<S>
 where
     S::ReadHalf: AsyncRead + Unpin,
 {
     fn poll_read(
-        mut self: Pin<&mut Self>,
+        self: Pin<&mut Self>,
         cx: &mut Context<'_>,
         buf: &mut [u8],
     ) -> Poll<io::Result<usize>> {
-        replace_waker(self.as_mut().project().read_waker, cx.waker());
-        loop {
-            let this = self.as_mut().project();
-            poll_future_would_block!(
-                cx,
-                this.read_waker,
-                io::Read::read(this.read_inner.get_mut(), buf),
-                self.as_mut().poll_read_impl()
-            )
-        }
+        self.project().read_inner.poll_read(cx, buf)
     }
 }
 
@@ -309,20 +258,11 @@ where
     ///
     /// On success, returns `Poll::Ready(Ok(num_bytes_read))`.
     pub fn poll_read_uninit(
-        mut self: Pin<&mut Self>,
+        self: Pin<&mut Self>,
         cx: &mut Context<'_>,
         buf: &mut [MaybeUninit<u8>],
     ) -> Poll<io::Result<usize>> {
-        replace_waker(self.as_mut().project().read_uninit_waker, cx.waker());
-        loop {
-            let this = self.as_mut().project();
-            poll_future_would_block!(
-                cx,
-                this.read_uninit_waker,
-                this.read_inner.get_mut().read_buf_uninit(buf),
-                self.as_mut().poll_read_impl()
-            )
-        }
+        self.project().read_inner.poll_read_uninit(cx, buf)
     }
 }
 
@@ -330,24 +270,12 @@ impl<S: Splittable + 'static> futures_util::AsyncBufRead for AsyncStream<S>
 where
     S::ReadHalf: AsyncRead + Unpin,
 {
-    fn poll_fill_buf(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<&[u8]>> {
-        replace_waker(self.as_mut().project().read_buf_waker, cx.waker());
-        loop {
-            let this = self.as_mut().project();
-            poll_future_would_block!(
-                cx,
-                this.read_buf_waker,
-                // SAFETY: The buffer won't be accessed after the future is ready, and the future
-                // won't live longer than the stream.
-                io::BufRead::fill_buf(this.read_inner.get_mut())
-                    .map(|s| unsafe { extend_lifetime(s) }),
-                self.as_mut().poll_read_impl()
-            )
-        }
+    fn poll_fill_buf(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<&[u8]>> {
+        self.project().read_inner.poll_fill_buf(cx)
     }
 
     fn consume(self: Pin<&mut Self>, amt: usize) {
-        io::BufRead::consume(self.project().read_inner.get_mut(), amt)
+        self.project().read_inner.consume(amt)
     }
 }
 
@@ -431,96 +359,24 @@ impl<S: AsyncRead + Unpin + 'static> futures_util::AsyncBufRead for AsyncReadStr
     }
 }
 
-impl<S: Splittable + 'static> AsyncStream<S>
-where
-    S::WriteHalf: AsyncWrite + Unpin,
-{
-    fn poll_flush_impl(self: Pin<&mut Self>) -> Poll<io::Result<usize>> {
-        let this = self.project();
-        // SAFETY:
-        // - The future won't live longer than the stream.
-        // - The stream is internally mutable.
-        // - The future only accesses the corresponding buffer and fields.
-        //   - No access overlap between the futures.
-        let inner = unsafe { extend_lifetime_mut(this.write_inner.get_mut()) };
-        let arr = WakerArray([
-            this.write_waker.as_ref().cloned(),
-            this.flush_waker.as_ref().cloned(),
-            this.close_waker.as_ref().cloned(),
-        ]);
-        let waker = Waker::from(Arc::new(arr));
-        let cx = &mut Context::from_waker(&waker);
-        let res = poll_future!(this.write_future, cx, inner.flush_write_buf());
-        Poll::Ready(res)
-    }
-
-    fn poll_close_impl(self: Pin<&mut Self>) -> Poll<io::Result<()>> {
-        let this = self.project();
-        // SAFETY:
-        // - The future won't live longer than the stream.
-        // - The stream is internally mutable.
-        // - The future only accesses the corresponding buffer and fields.
-        //   - No access overlap between the futures.
-        let inner = unsafe { extend_lifetime_mut(this.write_inner.get_mut()) };
-        let arr = WakerArray([
-            this.write_waker.as_ref().cloned(),
-            this.flush_waker.as_ref().cloned(),
-            this.close_waker.as_ref().cloned(),
-        ]);
-        let waker = Waker::from(Arc::new(arr));
-        let cx = &mut Context::from_waker(&waker);
-        let res = poll_future!(this.shutdown_future, cx, inner.get_mut().shutdown());
-        Poll::Ready(res)
-    }
-}
-
 impl<S: Splittable + 'static> futures_util::AsyncWrite for AsyncStream<S>
 where
     S::WriteHalf: AsyncWrite + Unpin,
 {
     fn poll_write(
-        mut self: Pin<&mut Self>,
+        self: Pin<&mut Self>,
         cx: &mut Context<'_>,
         buf: &[u8],
     ) -> Poll<io::Result<usize>> {
-        replace_waker(self.as_mut().project().write_waker, cx.waker());
-        if self.shutdown_future.is_some() {
-            debug_assert!(self.write_future.is_none());
-            ready!(self.as_mut().poll_close_impl())?;
-        }
-        loop {
-            let this = self.as_mut().project();
-            poll_future_would_block!(
-                cx,
-                this.write_waker,
-                io::Write::write(this.write_inner.get_mut(), buf),
-                self.as_mut().poll_flush_impl()
-            )
-        }
+        self.project().write_inner.poll_write(cx, buf)
     }
 
-    fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
-        replace_waker(self.as_mut().project().flush_waker, cx.waker());
-        if self.shutdown_future.is_some() {
-            debug_assert!(self.write_future.is_none());
-            ready!(self.as_mut().poll_close_impl())?;
-        }
-        let res = ready!(self.as_mut().poll_flush_impl());
-        self.project().flush_waker.take();
-        Poll::Ready(res.map(|_| ()))
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        self.project().write_inner.poll_flush(cx)
     }
 
-    fn poll_close(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
-        replace_waker(self.as_mut().project().close_waker, cx.waker());
-        // Avoid shutdown on flush because the inner buffer might be passed to the
-        // driver.
-        if self.write_future.is_some() || self.write_inner.has_pending_write() {
-            debug_assert!(self.shutdown_future.is_none());
-            ready!(self.as_mut().poll_flush_impl())?;
-        }
-        let res = ready!(self.as_mut().poll_close_impl());
-        self.project().close_waker.take();
-        Poll::Ready(res)
+    fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        self.project().write_inner.poll_close(cx)
     }
 }
 
@@ -618,24 +474,7 @@ impl<S: Splittable> Splittable for AsyncStream<S> {
     type WriteHalf = AsyncWriteStream<S::WriteHalf>;
 
     fn split(self) -> (Self::ReadHalf, Self::WriteHalf) {
-        let read_half = AsyncReadStream {
-            inner: self.read_inner,
-            read_future: self.read_future,
-            read_waker: self.read_waker,
-            read_uninit_waker: self.read_uninit_waker,
-            read_buf_waker: self.read_buf_waker,
-            _p: PhantomPinned,
-        };
-        let write_half = AsyncWriteStream {
-            inner: self.write_inner,
-            write_future: self.write_future,
-            shutdown_future: self.shutdown_future,
-            write_waker: self.write_waker,
-            flush_waker: self.flush_waker,
-            close_waker: self.close_waker,
-            _p: PhantomPinned,
-        };
-        (read_half, write_half)
+        (self.read_inner, self.write_inner)
     }
 }
 
