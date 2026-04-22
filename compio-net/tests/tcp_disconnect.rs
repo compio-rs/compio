e53547079d13695a83f0b2227c604ca527c6da95
Yuyi Wang <Strawberry_Str@hotmail.com>
2026-04-22 22:12:52 +0800

    test(net): connect reuse

--- compio-net/tests/tcp_disconnect.rs
+++ compio-net/tests/tcp_disconnect.rs
@@ -26,3 +26,29 @@ fn disconnect() {
         task.await.resume_unwind().expect("shouldn't be cancelled");
     })
 }
+
+#[test]
+fn reuse() {
+    compio_runtime::Runtime::new().unwrap().block_on(async {
+        let listener1 = TcpListener::bind("127.0.0.1:0").await.unwrap();
+        let addr1 = listener1.local_addr().unwrap();
+        let listener2 = TcpListener::bind("127.0.0.1:0").await.unwrap();
+        let addr2 = listener2.local_addr().unwrap();
+
+        let task = compio_runtime::spawn(async move {
+            let (socket, _) = listener1.accept().await.unwrap();
+            let socket = socket.disconnect().await.unwrap();
+            let (mut socket, _) = listener2.accept_with(socket).await.unwrap();
+            socket.shutdown().await.unwrap();
+            socket.close().await.unwrap();
+        });
+
+        let client = TcpStream::connect(addr1).await.unwrap();
+        let client = client.disconnect().await.unwrap();
+        let mut client = client.connect(addr2).await.unwrap();
+        client.shutdown().await.unwrap();
+        client.close().await.unwrap();
+
+        task.await.resume_unwind().expect("shouldn't be cancelled");
+    })
+}
